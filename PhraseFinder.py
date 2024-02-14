7cafecf5dbd15008c048c0ac3b2a6aa472234f17
JaxonPittmn <159969578+JaxonPittmn@users.noreply.github.com>
2024-02-13 18:27:08 -0600

    Update PhraseFinder.py

--- PhraseFinder.py
+++ PhraseFinder.py
@@ -1,28 +1,25 @@
-def Challenge():
-	startPoint = input("Starting point:\n")
-	startPoint = startPoint.split(":")
-	ID = startPoint[0]
-	FileName = startPoint[1]
-	phraseLength = int(startPoint[2])
-	word = ""
-	newPhrase = ""
-	file = open(FileName,'r')
+startPoint = input("Starting point:\n")
+startPoint = startPoint.split(":")
+ID = startPoint[0]
+FileName = startPoint[1]
+phraseLength = int(startPoint[2])
+word = ""
+newPhrase = ""
+file = open(FileName,'r')
 
-	for i in range(phraseLength):
-		for line in file:
-			line = line.split(":")
-			if line[0] == ID:
-				word = line[1]
-				FileName = line[2]
-				if "\n" in line[3]:
-					ID = line[3][:-1]
-				else:
-					ID = line[3]
-				file.close()
-				file = open(FileName,'r')
-				newPhrase += word + " "
-				break
+for i in range(phraseLength):
+	for line in file:
+		line = line.split(":")
+		if line[0] == ID:
+			word = line[1]
+			FileName = line[2]
+			if "\n" in line[3]:
+				ID = line[3][:-1]
+			else:
+				ID = line[3]
+			file.close()
+			file = open(FileName,'r')
+			newPhrase += word + " "
+			break
 
-	print(newPhrase )
-
-Challenge()
+print(newPhrase )
